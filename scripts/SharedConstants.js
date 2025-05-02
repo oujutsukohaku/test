a7a19f29b33d8de144c4375f4fe0282e9fc6b21e
oujutsukohaku <154648659+oujutsukohaku@users.noreply.github.com>
2025-05-02 17:18:28 +0800

    Delete scripts directory

--- scripts/SharedConstants.js
+++
@@ -1,6 +0,0 @@
-const assetUrl = "https://viewer.shinycolors.moe";
-const usedFont = "theFont";
-const zhcnFont = "AlimamaShuHeiTi";
-const translate_master_list = "https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/story.json";
-const translate_CSV_url = 'https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv';
-const fontTimeout = 3000; // control font loading time 放弃字体加载超时时间
